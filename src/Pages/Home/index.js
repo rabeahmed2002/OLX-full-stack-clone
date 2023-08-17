74dc6d948f9189b47bd960779356b49e56263ef4
rabeahmed2002 <mrabe2024@gmail.com>
2023-08-17 17:17:05 +0500

    Banner completed

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from "../../Components/Navbar"
+import Banner from "../../Components/Banner"
 
 const index = () => {
   return (
     <div>
       <Navbar/>
+      <Banner/>
     </div>
   )
 }
